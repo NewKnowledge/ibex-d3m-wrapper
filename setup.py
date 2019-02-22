3f84e5eca3df9f164bf737445e125c32ff43a711
Steve Kramer <steve@newknowledge.io>
2019-02-22 14:29:45 -0600

    Updating ibex-d3m-wrappe to reference new version of base primitive d3m_ibex.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         'd3m_ibex >= 1.1.1'
     ],
     dependency_links=[
-        "git+https://github.com/NewKnowledge/d3m_ibex@66970d1bd6b23a2b2710e67ab713072d1fa6f806#egg=d3m_ibex-1.1.1"
+        "git+https://github.com/NewKnowledge/d3m_ibex@4238f42a874287317018c41411b1cf79508283e6#egg=d3m_ibex-1.1.1"
     ],
     entry_points={
         'd3m.primitives': [
